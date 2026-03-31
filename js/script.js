4048eb6d0bbe3a74a891f37c80e5bf341cdebc3f
Дмитрий <dkovalkov731@gmail.com>
2026-03-31 23:47:29 +0300

    Добавил 3 лекции и исправил проблему с footer

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('.theme-btn');
-  if (!btn) return;
+document.addEventListener('DOMContentLoaded', () => { // ждём загрузки страницы
+  const btn = document.querySelector('.theme-btn'); // кнопка
+  if (!btn) return; // если нет кнопки, то выходим
 
   // применяем сохранённую тему
-  const saved = localStorage.getItem('theme');
-  const isDark = saved === 'dark';
-  document.body.classList.toggle('dark-theme', isDark);
+  const saved = localStorage.getItem('theme'); // сохранённая тема
+  const isDark = saved === 'dark'; // темная ли она?
+  document.body.classList.toggle('dark-theme', isDark); // добавляем класс
 
   // иконка
-  btn.textContent = isDark ? '☀️' : '🌙';
+  btn.textContent = isDark ? '☀️' : '🌙'; // меняем иконку
 
-  btn.addEventListener('click', (e) => {
-    e.preventDefault();
+  btn.addEventListener('click', (e) => { // по клику
+    e.preventDefault(); // отменяем действие по умолчанию
 
-    const nowDark = document.body.classList.toggle('dark-theme');
-    localStorage.setItem('theme', nowDark ? 'dark' : 'light');
-    btn.textContent = nowDark ? '☀️' : '🌙';
-  });
-});
+    const nowDark = document.body.classList.toggle('dark-theme'); // меняем класс
+    localStorage.setItem('theme', nowDark ? 'dark' : 'light'); // сохраняем
+    btn.textContent = nowDark ? '☀️' : '🌙'; // меняем иконку
+  }); 
+}); 
