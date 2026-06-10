f973561087433d2364b1cbb39d48407cf1f92b89
Дмитрий <dkovalkov731@gmail.com>
2026-06-10 11:44:34 +0300

    Добавление новых функция для окна профиля и исправление ошибок

--- js/profile.js
+++ js/profile.js
@@ -18,18 +18,45 @@ const profileBtn = document.getElementById('profile_link');
                     modalProfile.classList.add('modalProfile');
                     modalProfile.id = 'profile-modal';
 
+                    const regDate = users[userEmail]?.registeredAt
+                    const formattedDate = regDate ? new Date(regDate).toLocaleDateString() : 'не указана';
+
                     modalProfile.innerHTML = `
                         <div class="modal-content">
                             <span class="close">&times;</span>
                             <h2>Профиль</h2>
                             <p><strong>Почта:</strong> ${userEmail}</p>
-                            <p><strong>Пароль:</strong> ${userPassword}</p>
+                            <div class="passwordDiv">
+                            <p id="passwordID"><strong>Пароль:</strong> ${userPassword}</p>
+                            <button id="unlockBtn">Скрыть</button>
+                            <br>
+                            <p><strong>Дата регистрации:</strong> ${formattedDate}</p>
+                            <p><strong>Последний вход:</strong> ${new Date().toLocaleDateString()}</p>
+                            <br>
                             <button id="exitBtn" class="exitBtn">Выйти</button>
                         </div>
                     `;
                     
                     document.body.appendChild(modalProfile);
 
+                    // СКРЫТЬ ПАРОЛЬ И ЗАМЕНИТЬ НА ЗВЕЗДОЧКИ ПРИ НАЖАТИИ НА КНОПКУ
+                    const unlockBtn = modalProfile.querySelector('#unlockBtn');
+                    let isPasswordVisible = false;
+
+                    unlockBtn.addEventListener('click', () => {
+                        const passwordElement = modalProfile.querySelector('#passwordID');
+                        
+                        if (!isPasswordVisible) {
+                                passwordID.innerHTML = `<strong>Пароль:</strong> ${userPassword}` 
+                                isPasswordVisible = true;
+                                unlockBtn.textContent = 'Скрыть';
+                            } else {
+                                passwordElement.innerHTML = `<strong>Пароль:</strong> <span class="password">**********</span>`;
+                                isPasswordVisible = false;
+                                unlockBtn.textContent = 'Показать';
+                            }
+                        });
+                            
                     // ЗАКРЫТИЕ ПО КРЕСТИКУ
                     const closeBtn = modalProfile.querySelector('.close');
                     closeBtn.onclick = function() {
